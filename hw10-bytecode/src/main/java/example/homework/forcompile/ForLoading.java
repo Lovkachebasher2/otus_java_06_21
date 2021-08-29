4e1aed8ae7a1df586c3fe00f0ea9787e26d9e58a
roman.karavashkin <r.karavashkin@infomaximum-llc.ru>
2021-08-30 01:02:11 +0300

    first commit hw10: add buildSrc module + asm

---
+++ hw10-bytecode/src/main/java/example/homework/forcompile/ForLoading.java
@@ -0,0 +1,7 @@
+package example.homework.forcompile;
+
+public class ForLoading {
+    public void action() {
+        System.out.println("Hello, mr.Anderson");
+    }
+}
---
+++ hw10-bytecode/src/main/java/example/homework/forcompile/OneToOne.java
@@ -0,0 +1,7 @@
+package example.homework.forcompile;
+
+public class OneToOne {
+    public int add(int x, int y) {
+        return x + y;
+    }
+}
